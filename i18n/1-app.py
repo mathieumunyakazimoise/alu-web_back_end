a0ec822db3ee9a8d69b68efb098c212646862f76
mathieumunyakazimoise <124375301+mathieumunyakazimoise@users.noreply.github.com>
2024-10-18 21:30:44 +0000

    moiju

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' Flask app '''
+"""Basic Flask app"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
@@ -9,7 +9,7 @@ babel = Babel(app)
 
 
 class Config:
-    ''' App config '''
+    """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,11 +18,11 @@ class Config:
 app.config.from_object(Config)
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
-def hello_world():
-    ''' return the template '''
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """Home page"""
     return render_template('1-index.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
