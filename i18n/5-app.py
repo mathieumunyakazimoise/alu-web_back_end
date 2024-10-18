8063c1216ba9f1eefb044350bc173aad8c51f4ab
mathieumunyakazimoise <124375301+mathieumunyakazimoise@users.noreply.github.com>
2024-10-18 21:35:51 +0000

    moiju

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""
-A Flask web application that mocks a user login system and displays.
-"""
+"""Basic Flask app that implements i18n and internationalization"""
 
-from flask import Flask, request, g, render_template
-from typing import Optional, Dict, Any
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
 
 app = Flask(__name__)
 
-users: Dict[int, Dict[str, Optional[str]]] = {
+users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
@@ -16,36 +14,52 @@ users: Dict[int, Dict[str, Optional[str]]] = {
 }
 
 
-def get_user() -> Optional[Dict[str, Any]]:
-    """
-    Get user by ID from the users dictionary.
-    :return: A user dictionary if found, otherwise None.
-    """
+class Config:
+    """Config class for your application, it deals with babel mostly"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale for your application"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """Home page for your application"""
+    login = False
+    if g.get('user'):
+        login = True
+    return render_template('5-index.html', login=login)
+
+
+def get_user():
+    """Get user information from users dict"""
     try:
-        user_id: Optional[str] = request.args.get('login_as')
-        if user_id is not None:
-            return users.get(int(user_id))
-    except (ValueError, TypeError):
+        login_as = int(request.args.get('login_as'))
+        return users.get(int(login_as))
+    except Exception:
         return None
-    return None
 
 
 @app.before_request
-def before_request() -> None:
-    """
-    Set user information globally before each request.
-    """
-    g.user = get_user()
-
-
-@app.route('/')
-def index() -> str:
-    """
-    Render the index page.
-    :return: Rendered HTML content.
-    """
-    return render_template('5-index.html')
+def before_request():
+    """Before request"""
+    user = get_user()
+    print(user)
+    if user:
+        g.user = user
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
